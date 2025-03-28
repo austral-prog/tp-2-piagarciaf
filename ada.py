4d3f0a6b2b4cfb73122eebedbd6f8561432bdc43
piagarciaf <pgarciafernandez@mail.austral.edu.ar>
2025-03-28 12:33:00 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -5,3 +5,5 @@ def ada():
     print (first_name.title() , last_name.title())
     print (first_name.upper() , last_name.upper())
     print (f"\t{first_name.lower()} {last_name.lower()}")
+
+ada() 
