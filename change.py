049b52ac58d8be2b225b9cdf96ab9e41fac3aa02
piagarciaf <pgarciafernandez@mail.austral.edu.ar>
2025-04-03 11:48:29 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,12 +1,16 @@
 def change():
-    pesos= 100
+	pesos= 100
 	gasto= 23.75
-	vuelto= pesos - gasto #con centavos inluidos 
+	vuelto= pesos - gasto #con centavos inluidos
 	vueltosin= int(pesos - gasto) #num redondo
 	centavos= int((vuelto - vueltosin)* 100) 
-	print (f"Ingresar gasto:\n{gasto}")
-	print (f"Dinero recibido\n{pesos}")
-	print ("\n\nVuelto")
-	print(f"\n\nPesos:\n{vueltosin}")
-	print(f"Centavos:\n{centavos}")
-change()
+	print ("Ingresar gasto:")
+	print(gasto)
+
+	print ("Dinero recibido")
+	print(pesos)
+	print ("\nVuelto\n")
+	print("Pesos:")
+	print(vueltosin)
+	print("Centavos:")
+	print(centavos)
